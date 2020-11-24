a4688ed3691d33594ed6f2f2161ea192c1c8539d
Meenakshi003 <74905495+Meenakshi003@users.noreply.github.com>
2020-11-24 13:11:48 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,30 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=3
+>>> first_number=a
+>>> second_number=10
+>>> print(first_number/second_number)
+0.3
+>>> b=2
+>>> first_number=b
+>>> second_number=50
+>>> print(first_number*second_number)
+100
+>>> c=4
+>>> first_number=c
+>>> second_number=60
+>>> print(first_number+second_number)
+64
+>>> A='python'
+>>> A=A.replace('h','g')
+>>> A
+'pytgon'
+>>> a=3
+>>> b=6.7
+>>> a=float(a)
+>>> a
+3.0
+>>> b=int(b)
+>>> b
+6
+>>> 
