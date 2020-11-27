365c62adc4cc7c1150656a3aa0972a610e5abed2
Meenakshi003 <74905495+Meenakshi003@users.noreply.github.com>
2020-11-27 20:53:00 +0530

    Add files via upload

---
+++ task5a.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+def arithmetic(x,y):
+    print("addition of two numbers is:",x+y)
+    print("subtraction of two numbers is:",x-y)
+    print("multiplication of two numbers is:",x*y)
+    print("division of two numbers is:",x/y)
+arithmetic(a,b)    
+    
---
+++ task5b.py
@@ -0,0 +1,6 @@
+def covid(patient_name,temp="98 deg"):
+    print("the patient name is",patient_name,"and temperature is",temp)
+covid("Jerry")
+covid("Tom")
+covid("Tarzan")
+covid("Ben","100 deg")
