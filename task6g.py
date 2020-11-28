06200cc1adf504064daf73c89b7dfd08249f8a56
Meenakshi003 <74905495+Meenakshi003@users.noreply.github.com>
2020-11-28 20:08:55 +0530

    Add files via upload

---
+++ task6g.py
@@ -0,0 +1,4 @@
+days=int(input("enter the number:"))
+years=days/365
+print("the years is:")
+print(years)               
---
+++ task6h.py
@@ -0,0 +1,7 @@
+import math
+a=math.pi/6
+print("the value of sine pi/6 is:",end="")
+print(math.sin(a))
+print("the value of cosine pi/6 is",end="")
+print(math.cos(a))
+
